52f00d2204e2b54ac690675dc863a4545b559e66
linchangjian <95537559@qq.com>
2019-07-28 21:56:25 +0800

    添加了flex简单布局

--- src/features/flexlayout/FlexLayoutPage.tsx
+++ src/features/flexlayout/FlexLayoutPage.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { Text } from 'react-native';
-
-
-
+import { Text, ScrollView, StyleSheet, View } from 'react-native';
 
 export class FlexLayoutPage extends React.Component{
     render(){
       return(
-          <Text>FlexLayoutPage</Text>
+          <View style={styles.container}>
+          
+          <Text style={styles.box_red}></Text>
+          <Text style={styles.box_blue}></Text>
+        
+          </View>
       );  
     };
 }
+
+const styles = StyleSheet.create({
+    box_red:{
+        flex:1,
+        width:40,
+        height:40,
+        backgroundColor:"#FF0000"
+    },
+    box_blue:{
+        flex:1,
+        width:40,
+        height:40,
+        backgroundColor:"#0000FF"
+    },
+    box_green:{
+        width:40,
+        height:40,
+        backgroundColor:"#00FF00"
+    },
+    container:{
+        flex:1,
+        justifyContent:"center",
+        flexDirection:"row"
+    }
+});
